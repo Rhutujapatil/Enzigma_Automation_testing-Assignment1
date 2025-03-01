cea30ce0813c21df3e8374ea51e34897d04c02aa
Rhutujapatil <158797785+Rhutujapatil@users.noreply.github.com>
2025-03-01 11:10:42 +0530

    SignupPageValidationPage.java

--- SignupPageValidationPage.java
+++ SignupPageValidationPage.java
@@ -1,36 +1,43 @@
 package com.AllPages;
-import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import java.time.Duration;
 
-public class SignupPageValidationPage {
-	
-	WebDriver driver;
-	public SignupPageValidationPage(WebDriver driver)
-	{
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
-	}
-	
-	@FindBy(xpath = "//*[@class=\"slds-float_right\"]/a")
-	WebElement signup;
-	
-	@FindBy(xpath ="//*[@id=\"id_17364210188447499\"]")
-	WebElement signupp;
-	
-	@FindBy(xpath ="//*[@id=\"modal-content-id-1\"]/abx-sign-up/div/div/div[2]/span/div[2]/abx-button/button")
-	WebElement procedpath;
+public class SignupPageValidation {
+    WebDriver driver;
+    WebDriverWait wait;
 
-   public void SignupPageValidationPage(String emaillll) throws InterruptedException
-   {
-	   Thread.sleep(3000);
-	   signup.click();
-	   signupp.sendKeys(emaillll);
-	   procedpath.click();
-	   
-     }
+    public SignupPageValidation(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
 
+    @FindBy(xpath = "//*[@class='slds-float_right']/a")
+    WebElement signupButton;
+
+    @FindBy(xpath = "//*[@id='id_17364210188447499']")
+    WebElement emailField;
+
+    @FindBy(xpath = "//*[@id='modal-content-id-1']/abx-sign-up/div/div/div[2]/span/div[2]/abx-button/button")
+    WebElement proceedButton;
+
+    @FindBy(xpath = "//*[contains(text(),'Account created successfully!')]") // Update based on actual UI
+    WebElement successMessage;
+
+    public void validateSignup(String email) {
+        wait.until(ExpectedConditions.elementToBeClickable(signupButton)).click();
+        wait.until(ExpectedConditions.visibilityOf(emailField)).sendKeys(email);
+        wait.until(ExpectedConditions.elementToBeClickable(proceedButton)).click();
+
+        // Validation
+        wait.until(ExpectedConditions.visibilityOf(successMessage));
+        Assert.assertEquals(successMessage.getText(), "Account created successfully!", "Signup failed!");
+    }
 }
