0fe1dcf6c24c00a71b6fa75b247104f493918f32
Rhutujapatil <158797785+Rhutujapatil@users.noreply.github.com>
2025-03-01 11:05:48 +0530

    BaseTest.java

--- BaseTest.java
+++ BaseTest.java
@@ -7,27 +7,20 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-	
-	protected WebDriver driver;
-	
-	@BeforeMethod
-	  public void beforeMethod() throws InterruptedException {
+    protected WebDriver driver;
 
-		driver = new ChromeDriver();
-		driver.get("https://app-staging.nokodr.com/");
-		Thread.sleep(2000);
-		driver.manage().window().maximize();
-		Thread.sleep(2000);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	}
-	
-	@AfterMethod
-	  public void afterMethod() throws InterruptedException
-	  {
-		Thread.sleep(6000);
-		driver.quit();		
-	}
-	 
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://app-staging.nokodr.com/");
+    }
 
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
-    
