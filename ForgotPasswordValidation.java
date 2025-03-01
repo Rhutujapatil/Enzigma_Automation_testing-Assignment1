b4306b49edaa559c3689dcc7b9ffb250e734b3f7
Rhutujapatil <158797785+Rhutujapatil@users.noreply.github.com>
2025-03-01 11:07:41 +0530

    Update ForgotPasswordValidation.java

--- ForgotPasswordValidation.java
+++ ForgotPasswordValidation.java
@@ -1,39 +1,43 @@
 package com.AllPages;
-import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import java.time.Duration;
 
 public class ForgotPasswordValidation {
-	
-	WebDriver driver;
-	public ForgotPasswordValidation(WebDriver driver)
-	{
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
-	}
-	
-	@FindBy(xpath ="/html/body/app-root/login/abx-auth-container/div/div[2]/div/abx-login/div/a")
-	WebElement forgot;
-	
-	@FindBy(xpath ="//*[@id=\"id_17364173266215197\"]")
-	WebElement emailField;
-	
-	@FindBy(xpath ="//*[@title=\"Proceed\"]")
-	WebElement proced;
-	
-	
+    WebDriver driver;
+    WebDriverWait wait;
 
-   public void ForgotPasswordValidation(String email) throws InterruptedException
-   {
-	   Thread.sleep(3000);
-	   forgot.click();
-	   emailField.sendKeys(email);
-	   proced.click();
-	
-	   
-     }
+    public ForgotPasswordValidation(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
 
+    @FindBy(xpath = "/html/body/app-root/login/abx-auth-container/div/div[2]/div/abx-login/div/a")
+    WebElement forgotPasswordLink;
+
+    @FindBy(xpath = "//*[@id='id_17364173266215197']")
+    WebElement emailInputField;
+
+    @FindBy(xpath = "//*[@title='Proceed']")
+    WebElement proceedButton;
+
+    @FindBy(xpath = "//*[contains(text(),'Reset link sent to your email')]") // Update based on actual UI
+    WebElement successMessage;
+
+    public void validateForgotPassword(String email) {
+        wait.until(ExpectedConditions.elementToBeClickable(forgotPasswordLink)).click();
+        wait.until(ExpectedConditions.visibilityOf(emailInputField)).sendKeys(email);
+        wait.until(ExpectedConditions.elementToBeClickable(proceedButton)).click();
+
+        // Validation
+        wait.until(ExpectedConditions.visibilityOf(successMessage));
+        Assert.assertEquals(successMessage.getText(), "Reset link sent to your email", "Forgot password failed!");
+    }
 }
